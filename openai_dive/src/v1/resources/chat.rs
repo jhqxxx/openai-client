33ccaf26123446103da87120fe348507567525a7
jhqxxx <18280426169@163.com>
2025-12-25 18:51:22 +0800

    update ChatMessageContentPart

--- openai_dive/src/v1/resources/chat.rs
+++ openai_dive/src/v1/resources/chat.rs
@@ -595,11 +595,15 @@ pub enum ChatMessageContent {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-#[serde(tag = "type", rename_all = "lowercase")]
+#[serde(tag = "type")]
 pub enum ChatMessageContentPart {
+    #[serde(rename = "text")]
     Text(ChatMessageTextContentPart),
+    #[serde(rename = "image")]
     Image(ChatMessageImageContentPart),
+    #[serde(rename = "audio")]
     Audio(ChatMessageAudioContentPart),
+    #[serde(rename = "video")]
     Video(ChatMessageVideoContentPart),
 }
 
